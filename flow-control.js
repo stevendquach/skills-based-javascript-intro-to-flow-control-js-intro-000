489f6a1987ac023dfc900e32f45c2eacfc22ab68
Steven Quach <stevendquach@gmail.com>
2020-08-05 08:20:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,9 @@ function teenager(age) {
 function ageChecker(age) {
   if (age>= 13 && age <= 19) {
     return ("You are a teenager!")
-  }
+}   else if (age <= 12) {
+      return ("You are a kid")
+}
 
 }
 
