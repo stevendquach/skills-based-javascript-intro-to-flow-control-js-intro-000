af11af6acb67fdc4a5f65a886469515fa54acad5
Steven Quach <stevendquach@gmail.com>
2020-08-05 10:04:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -35,13 +35,13 @@ function switchAge(age) {
     case (15):
       return "You are a teenager"
     case (16):
-      return ("You are a teenager")
+      return "You are a teenager"
     case (17):
-      return "You are a teenager")
+      return "You are a teenager"
     case (18):
-      return ("You are a teenager")
+      return "You are a teenager"
     case (19):
-      return ("You are a teenager")
+      return "You are a teenager"
     default:
       return "You have an age"
     }
