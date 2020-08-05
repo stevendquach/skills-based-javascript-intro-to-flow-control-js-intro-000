e6dee425bd03589741fd66919a99c2b967390edd
Steven Quach <stevendquach@gmail.com>
2020-08-05 09:11:01 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,7 +25,7 @@ function ternaryTeenager(age) {
 return (age >= 13 && age <= 19) ? ("You are a teenager") :
 ("You are not a teenager")
 }
-
+}
 function switchAge(age) {
 switch (age) {
   case (age >13 && age <= 19):
